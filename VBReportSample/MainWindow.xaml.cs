5411cc702943723a76f295dcc803ba80f1dbbcf5
紅焔 <doncot@gmail.com>
2016-02-15 16:08:37 +0900

    やり直し

--- VBReportSample/MainWindow.xaml.cs
+++ VBReportSample/MainWindow.xaml.cs
@@ -44,35 +44,69 @@ namespace VBReportSample
                 imageContainer.Source = WpfDrawingHelper.CreateBitmapImage(rawImage);
 
                 //VBReportのインスタンス
-                AdvanceSoftware.VBReport8.CellReport cellReportLocal = new AdvanceSoftware.VBReport8.CellReport();
+                using (var cellReportLocal = new AdvanceSoftware.VBReport8.CellReport())
+                {
+                    //エラーハンドラ
+                    cellReportLocal.Error += (object lsender, AdvanceSoftware.VBReport8.ReportErrorEventArgs le) =>
+                    {
+                        MessageBox.Show("cellReport1エラー：[" + System.Enum.GetName(typeof(AdvanceSoftware.VBReport8.ErrorNo), le.ErrorNo) + "]");
+                    };
+
+                    var sheetPath = AppDomain.CurrentDomain.BaseDirectory + "sample.xlsx";
+                    cellReportLocal.FileName = sheetPath;
+                    cellReportLocal.Report.Start();
+                    cellReportLocal.Report.File();
+                    cellReportLocal.Page.Start();
+
+                    //ピクセルで指定
+                    cellReportLocal.ScaleMode = ScaleMode.Pixel;
+
+                    cellReportLocal.Cell("B4").Drawing.AddImage(imagePath, originalImage.Width, originalImage.Height);
+
+                    cellReportLocal.Page.End();
+                    cellReportLocal.Report.End();
+                    cellReportLocal.Report.SaveAs("result_sample.xlsx", ExcelVersion.ver2013);
+
+#if DEBUG
+                    //作成したシートを開く
+                    Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\result_sample.xlsx");
+#endif
+                }
+            }
+        }
+
+        private void TryToOpenVBReportFromWrongPath_Click(object sender, RoutedEventArgs e)
+        {
+            //VBReportのインスタンス
+            using (var cellReportLocal = new AdvanceSoftware.VBReport8.CellReport())
+            {
                 //エラーハンドラ
+
                 cellReportLocal.Error += (object lsender, AdvanceSoftware.VBReport8.ReportErrorEventArgs le) =>
                 {
                     MessageBox.Show("cellReport1エラー：[" + System.Enum.GetName(typeof(AdvanceSoftware.VBReport8.ErrorNo), le.ErrorNo) + "]");
                 };
 
-                var sheetPath = AppDomain.CurrentDomain.BaseDirectory + "sample.xlsx";
-                cellReportLocal.FileName = sheetPath;
-                cellReportLocal.Report.Start();
-                cellReportLocal.Report.File();
-                cellReportLocal.Page.Start();
 
-                //ピクセルで指定
-                cellReportLocal.ScaleMode = ScaleMode.Pixel;
+                try
+                {
+                    //間違ったファイル名で開こうとする。
+                    cellReportLocal.FileName = AppDomain.CurrentDomain.BaseDirectory + "fdjafjdkajfjdkajfkdjkfjksfsafkjfkas.xlsx";
 
-                cellReportLocal.Cell("B4").Drawing.AddImage(imagePath, originalImage.Width, originalImage.Height);
+                    cellReportLocal.Report.Start();
+                    cellReportLocal.Report.File();
+                    cellReportLocal.Page.Start();
 
-                cellReportLocal.Page.End();
-                cellReportLocal.Report.End();
-                cellReportLocal.Report.SaveAs("result_sample.xlsx", ExcelVersion.ver2013);
+                    cellReportLocal.Page.End();
+                    cellReportLocal.Report.End();
+                }
+                catch(NullReferenceException)
+                {
+                    MessageBox.Show("NullReferenceExceptionがVBReportから投げられました。");
+                    //再スローする
+                    throw;
+                }
 
-#if DEBUG
-                //作成したシートを開く
-                Process.Start(Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\result_sample.xlsx");
-#endif
-
-                cellReportLocal.Dispose();
-                cellReportLocal = null;
             }
         }
     }
